49da2fd6e6d85e387dd9070ba0dc22d4beb3585f
Gabriel Lima <bielsantoslima10@gmail.com>
2021-03-20 15:04:49 -0400

    added events to load saved sequences and generate sequences

--- src/services/Interface.js
+++ src/services/Interface.js
@@ -1,30 +1,77 @@
 const Group = require('../components/Group')
+const SavedSequence = require('../components/SavedSequence')
 
 module.exports = function Interface(){
 	const DEFAULT_GROUP_SIZE = 1
+	const DEFAULT_SEQUENCE_SIZE = 1
 	const DEFAULT_DIV_GROUPS_VALUE = ''
-
-	const buttonRemoveGroup = document.querySelector('.bt-remove-group')
-	const buttonAddGroup = document.querySelector('.bt-add-group')
-	const buttonClearAllGroups = document.querySelector('.bt-clear-all-groups')
-	const divGroups = document.querySelector('.groups')
-	const inputGroupSize = document.querySelector('.ipt-group-size')
+	
 	let groupSize = DEFAULT_GROUP_SIZE
+	let sequenceSize = DEFAULT_SEQUENCE_SIZE
+
+	const buttonRemoveGroup = getDOMElement('.bt-remove-group')
+	const buttonAddGroup = getDOMElement('.bt-add-group')
+	const buttonGenerateSequences = getDOMElement('.bt-generate-sequences')
+	const buttonClearAllGroups = getDOMElement('.bt-clear-all-groups')
+	const divGroups = getDOMElement('.groups')
+	const divSavedSequences = getDOMElement('.saved-sequences')
+	const inputGroupSize = getDOMElement('.ipt-group-size')
+	const inputSequenceSize = getDOMElement('.ipt-sequence-size')
+	const textSizeOfGroups = getDOMElement('.size-of-groups')
 
 	function init(){	
 		addEvents()
+		loadSavedSequences()
 	}
 	
+	function getDOMElement(selectors){
+		return document.querySelector(selectors)
+	}
+
+	function loadSavedSequences(){
+		// loading from local storage
+		const savedSequences = [
+			{
+				createdAt : '21-03-2021',
+				time : '12:23',
+				sequences : [[1,2], [2,3], [1,3]]
+			},
+		]
+
+		for(const savedSequence of savedSequences){
+			const liSavedSequence = SavedSequence(savedSequence)
+			divSavedSequences.innerHTML += liSavedSequence
+		}
+	}
+
 	function addEvents(){
+		onGenerateSequences(buttonGenerateSequences)
 		addGroup(buttonAddGroup)
 		changeGroupSize(inputGroupSize)
 		removeGroup(buttonRemoveGroup)
 		clearAllGroups(buttonClearAllGroups)
+		setSequenceSize(inputSequenceSize)
+	}
+
+	function onGenerateSequences(button){
+		button.addEventListener('click', () => {
+			console.log(sequenceSize)
+			// get numbers of groups
+			// generate sequences
+			// show result
+		})
+	}
+
+	function setSequenceSize(input){
+		input.addEventListener('change', () => {
+			sequenceSize = input.value
+		})
 	}
 
 	function clearAllGroups(button){
 		button.addEventListener('click', ()=> {
 			divGroups.innerHTML = DEFAULT_DIV_GROUPS_VALUE
+			updateSizeOfGroups(textSizeOfGroups, 0)
 		})
 	}
 
@@ -44,13 +91,19 @@ module.exports = function Interface(){
 				title,
 				size : groupSize
 			})
+			updateSizeOfGroups(textSizeOfGroups, order)
 		})
 	}
 	
 	function removeGroup(button){
 		button.addEventListener('click', ()=> {
 			divGroups.lastElementChild.remove()
+			updateSizeOfGroups(textSizeOfGroups, divGroups.children.length)
 		})
+	}
+
+	function updateSizeOfGroups(element, value){
+		element.innerText = value
 	}
 
 	return {
